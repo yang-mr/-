476238913a9474ff02fd999328f8d86e1a237544
yangwei <yang971460565@126.com>
2017-11-01 00:03:14 +0800

    add popupWindow_Demo

--- WidgetDemo/popupwindowdemo/src/main/java/com/example/popupwindowdemo/MainActivity.java
+++ WidgetDemo/popupwindowdemo/src/main/java/com/example/popupwindowdemo/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.popupwindowdemo;
 
 import android.content.Context;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
@@ -17,16 +20,25 @@ import android.widget.SimpleAdapter;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private PopupWindow mPopupWindow;
+    private Button mBt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBt = (Button) findViewById(R.id.show);
+
     }
 
     public void show(View view) {
+        if (mPopupWindow == null)
+        mPopupWindow = new PopupWindow(this);
 
-        PopupWindow popupWindow = new PopupWindow(this);
+        if (mPopupWindow.isShowing()) {
+            mPopupWindow.dismiss();
+            return;
+        }
+
         View window_view = LayoutInflater.from(this).inflate(R.layout.window_view, null);
 
         GridView gridView = (GridView) window_view.findViewById(R.id.gridview);
@@ -35,16 +47,18 @@ public class MainActivity extends AppCompatActivity {
         gridView.setAdapter(myAdapter);
 
         //4者的属性必须设置
-        popupWindow.setWidth(ViewGroup.LayoutParams.MATCH_PARENT); //设置宽度 布局文件里设置的没有用
-        popupWindow.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);  //设置高度  必须代码设置
-        popupWindow.setContentView(window_view);  //设置布局
+        mPopupWindow.setWidth(ViewGroup.LayoutParams.MATCH_PARENT); //设置宽度 布局文件里设置的没有用
+        mPopupWindow.setHeight(ViewGroup.LayoutParams.MATCH_PARENT);  //设置高度  必须代码设置
+        mPopupWindow.setContentView(window_view);  //设置布局
         //popupWindow.showAsDropDown(window_view);  //设置显示位置
 
-        View parentView = LayoutInflater.from(this).inflate(R.layout.activity_main, null);
-        popupWindow.showAtLocation(parentView, Gravity.BOTTOM, 0, 0);
+        mPopupWindow.setBackgroundDrawable(new BitmapDrawable());
+        mPopupWindow.setOutsideTouchable(true); //外部是否可点击 点击外部消失
+        mPopupWindow.setFocusable(true);  //响应返回键  点击返回键 消失
 
-        //设置动画
-        popupWindow.setAnimationStyle(R.style.AnimTheme);
+        View parentView = LayoutInflater.from(this).inflate(R.layout.activity_main, null);
+        mPopupWindow.showAtLocation(parentView, Gravity.BOTTOM, 0, 0);      //相对父布局
+        // mPopupWindow.showAsDropDown(mBt, 0, 0);   //相对某个控件显示
     }
 
     private class MyAdapter extends BaseAdapter {
