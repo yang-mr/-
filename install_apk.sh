65a76208b99bea56f036b16b35c49d0a715217bf
jack <yang971460565@163.com>
2018-01-22 10:30:01 +0800

    add report

--- install_apk.sh
+++ install_apk.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 adb connect 192.168.50.211
-adb -s 192.168.50.211:5555 uninstall com.cafari.boxserver
+adb -s kylin uninstall com.cafari.boxserver
 
-adb -s 192.168.50.211:5555 shell "rm -rR /storage/emulated/0/cafari"
+adb -s kylin shell "rm -rR /storage/emulated/0/cafari"
 echo "remove file success "
 #rm -rR /storage/emulated/0/cafari
 
 #adb exit
 
-adb -s 192.168.50.211:5555 install -r /home/jack/Downloads/app-armeabi-v7a-debug.apk
+adb -s kylin install -r /home/jack/Downloads/app-armeabi-v7a-debug.apk
 
-adb -s 192.168.50.211:5555 shell am start -n com.cafari.boxserver/com.cafari.boxserver.ui.LaunchActivity
+adb -s kylin shell am start -n com.cafari.boxserver/com.cafari.boxserver.ui.LaunchActivity
 
 #obtain mac address 
 #adb shell  cat /sys/class/net/wlan0/address
